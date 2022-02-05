c8081e2cd62a22fedf44f3ba7ec954e1827fd491
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-05 17:27:41 +0300

    generate a class to create quotes and define its logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes: string[];
+
+  constructor(){
+    this.quotes = ['Quote1', 'Quote2', 'Quote3', 'Quote4', 'Quote6']
+  }
+
+  
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { title } from 'process';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  constructor() { }
+  title = 'Share a Quote';
+  quotes:Quote[] = [
+    new Quote(1, 'Jay', 'Dreams', 'When you have a dream, you’ve got to grab it and never let go.', 'Carol Burnett', new Date(2022,2,5), 0,0),
+    new Quote(2, 'Lucy', 'Willpower', 'Nothing is impossible. The word itself says ‘I’m possible!', 'Audrey Hepburn', new Date(2022,2,4), 0,0),
+    new Quote(3, 'Jack', 'Willpower', 'There is nothing impossible to they who will try.', 'Alexander the Great', new Date(2022,2,3), 0,0),
+    new Quote(4, 'Abdul', 'Time', 'The bad news is time flies. The good news is you are the pilot.', 'Michael Altshuler', new Date(2022,2,2), 0,0),
+    new Quote(5, 'Wanjiru', 'Hardship', 'Keep your face always toward the sunshine, and shadows will fall behind you.', 'Walt Whitman', new Date(2022,2,2),0,0),
+    new Quote(6, 'Othuol', 'Success', 'Success is not final, failure is not fatal: it is the courage to continue that counts', new Date(2022,2,1),0,0)
+  ];
 
-  ngOnInit(): void {
-  }
-
+  
 }
