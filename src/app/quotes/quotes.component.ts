b2496c6628ebfd5efef5dfa999df627b8d62a20f
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-07 18:46:10 +0300

    update the quote function to properly display quote votes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote('Jay', 'Dreams', 'When you have a dream, you’ve got to grab it and never let go.', 'Carol Burnett', new Date(2022,2,5), 0,0),
-    new Quote('Lucy', 'Willpower', 'Nothing is impossible. The word itself says ‘I’m possible!', 'Audrey Hepburn', new Date(2022,2,4), 0,0),
-    new Quote('Jack', 'Willpower', 'There is nothing impossible to they who will try.', 'Alexander the Great', new Date(2022,2,3), 0,0),
-    new Quote('Abdul', 'Time', 'The bad news is time flies. The good news is you are the pilot.', 'Michael Altshuler', new Date(2022,2,2), 0,0),
-    new Quote('Wanjiru', 'Hardship', 'Keep your face always toward the sunshine, and shadows will fall behind you.', 'Walt Whitman', new Date(2022,2,2),0,0),
-    new Quote('Othuol', 'Success', 'Success is not final, failure is not fatal: it is the courage to continue that counts', 'Winston Churchill', new Date(2022,2,1),0,0)
+    new Quote('Jay', 'Dreams', 'When you have a dream, you’ve got to grab it and never let go.', 'Carol Burnett', new Date(2022,2,5)),
+    new Quote('Lucy', 'Willpower', 'Nothing is impossible. The word itself says ‘I’m possible!', 'Audrey Hepburn', new Date(2022,2,4)),
+    new Quote('Jack', 'Willpower', 'There is nothing impossible to they who will try.', 'Alexander the Great', new Date(2022,2,3)),
+    new Quote('Abdul', 'Time', 'The bad news is time flies. The good news is you are the pilot.', 'Michael Altshuler', new Date(2022,2,2)),
+    new Quote('Wanjiru', 'Hardship', 'Keep your face always toward the sunshine, and shadows will fall behind you.', 'Walt Whitman', new Date(2022,2,2)),
+    new Quote('Othuol', 'Success', 'Success is not final, failure is not fatal: it is the courage to continue that counts', 'Winston Churchill', new Date(2022,2,1))
   ];
 
   toggleDetails(index: number){
@@ -27,24 +27,30 @@ export class QuotesComponent implements OnInit {
     }
   }
 
-  deleteQuote(isComplete: any, index: number){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+  // deleteQuote(isComplete: any, index: number){
+  //   if (isComplete) {
+  //     let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
-      if (toDelete){
-        this.quotes.splice(index,1)
-      }
-    }
+  //     if (toDelete){
+  //       this.quotes.splice(index,1)
+  //     }
+  //   }
+  // }
+
+  deleteQuote(index: number){
+    this.quotes.splice(index,1)
   }
 
-  addNewQuote(isComplete: any, index: number){
-    if (isComplete) {
-      let toAddNewQuote = confirm(`Are you sure you want to add ${this.quotes[index].name}?`)
+  addQuote(quote:Quote){
+    // if () {
+    //   let toAddQuote = confirm(`Are you sure you want to add ${this.quotes[index].name}?`)
 
-      if (toAddNewQuote){
-        this.quotes.splice(index,1)
-      }
-    }
+    //   if (toAddQuote){
+    //     this.quotes.splice(index,1)
+    //   }
+    // }
+
+    this.quotes.push(quote)
   }
 
   // addNewQuote(quote: Quote){
