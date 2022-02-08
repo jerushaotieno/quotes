a0dfebed704dc837e05d03fcf7c24879f6b14781
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-08 13:22:39 +0300

    update date logic to show number of days since a quote posting

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -11,9 +11,9 @@ export class DateCountPipe implements PipeTransform {
     var dateDifference = Math.abs(todayWithNoTime - value) //returns value in miliseconds
     const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    var dateCounter = Math.floor(dateDifferenceSeconds/secondsInDay);
 
-    if (dateCounter >= 1 && value < todayWithNoTime){
+    if (dateCounter >= 1 && todayWithNoTime > value){
       return dateCounter;
     }else{
       return 0;
